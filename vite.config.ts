247a1eb4a7d6a1328355d6ce5075111393fce10f
Anmol <anmolvermahere@gmail.com>
2025-12-28 02:36:27 +0530

    Chane directory issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
-  root: ".", // <-- ensure index.html is read from project root
-  publicDir: "public", // optional, default is "public"
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+});
